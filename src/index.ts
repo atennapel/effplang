1da3513b85a6840af9ed231d728ce0b3a0ecdc4c
Albert ten Napel <aptennap@gmail.com>
2019-06-10 23:21:27 +0200

    fresh start

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-/**
- * TODO:
- * - parser: effect rows
- * - operation calls
- * - handlers
- * - algebraic effects in machine
- * - definitions
- * - algebraic data types
- * - row polymorphism
- */
+console.log(42);
